c026430f5099533672960690664b892f2230e641
Saklyfiras <133059715+SaklyFiras1@users.noreply.github.com>
2026-03-06 17:20:54 +0100

    Initialize and start ZMQ Remote API server

--- start_zmq.lua
+++ start_zmq.lua
@@ -1,12 +1,9 @@
 -- start_zmq.lua
 function sysCall_init()
-    print("[START_ZMQ] Starting ZeroMQ remote API server...")
+    print("[START_ZMQ] Initializing ZMQ Remote API server...")
     
-    local addonHandle = sim.getScript(sim.scripttype_addonscript, -1, "ZeroMQ remote API server")
-    if addonHandle ~= -1 then
-        sim.callScriptFunction("sysCall_init", addonHandle)
-        print("[START_ZMQ] ZeroMQ remote API server initialized successfully")
-    else
-        print("[START_ZMQ] ERROR: 'ZeroMQ remote API server' add-on not found")
-    end
+    -- Start the ZMQ remote API server
+    simZMQ.startServer(23000)
+    
+    print("[START_ZMQ] ZMQ Remote API server started on port 23000")
 end
